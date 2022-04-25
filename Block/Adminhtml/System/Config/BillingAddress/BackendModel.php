4f08bb4ffd46aa6322028bf224e0b399dfde4894
Martin Knudsen <mkk@wexo.dk>
2022-04-25 10:11:21 +0200

    removed billing_addres since it is not allowed on rate_quotes

--- Block/Adminhtml/System/Config/BillingAddress/BackendModel.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Wexo\Webshipper\Block\Adminhtml\System\Config\BillingAddress;
-
-class BackendModel extends \Wexo\Webshipper\Block\Adminhtml\System\Config\AbstractBackendModel
-{
-}
